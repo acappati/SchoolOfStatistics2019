d51e95cfb24595cbf303bf6964a7ddf649eb126c
acappati <alessandra.cappati@cern.ch>
2019-06-07 11:47:27 +0200

    update bdt

--- MachineLearning_classes/train_trees.py
+++ MachineLearning_classes/train_trees.py
@@ -1,14 +1,10 @@
 import ROOT
 import os
-import argparse
 
-
-
-fIn      = ROOT.TFile("Higgs_data.root")
-tree_sig = fIn.Get("sig_tree")
+fIn = ROOT.TFile("Higgs_data.root")
 tree_bkg = fIn.Get("bkg_tree")
-fOut     = ROOT.TFile("output.root","RECREATE")
-
+tree_sig = fIn.Get("sig_tree")
+fOut = ROOT.TFile("myBDToutput.root","RECREATE")
 
 ROOT.TMVA.Tools.Instance()
 
@@ -17,17 +13,35 @@ factory = ROOT.TMVA.Factory("TMVAClassification", fOut,":".join(["!V","Transform
 dataloader = ROOT.TMVA.DataLoader()
 
 
-dataloader.AddVariable("lepton_pT", "F") # Both Float and Double variable types must be indicated as F
-dataloader.AddVariable("lepton_eta","F")
-dataloader.AddVariable("lepton_phi","F")
-dataloader.AddVariable("jet1_pt",   "F")
-dataloader.AddVariable("jet1_eta",  "F")
-dataloader.AddVariable("jet1_btag", "F")
-dataloader.AddVariable("jet2_pt",   "F")
-dataloader.AddVariable("jet2_eta",  "F")
-dataloader.AddVariable("jet2_btag", "F")
-dataloader.AddVariable("m_jj",      "F")
-
+# Both Float and Double variable types must be indicated as F
+dataloader.AddVariable("lepton_pT","F")
+# dataloader.AddVariable("lepton_eta","F") # 15
+# dataloader.AddVariable("lepton_phi","F") # 15
+dataloader.AddVariable("missing_energy_magnitude","F") # 15
+# dataloader.AddVariable("missing_energy_phi","F") # 15
+dataloader.AddVariable("jet1_pt","F")
+# dataloader.AddVariable("jet1_eta","F") # 15
+# dataloader.AddVariable("jet1_phi","F") # 15
+# dataloader.AddVariable("jet1_btag","F") # 10
+dataloader.AddVariable("jet2_pt","F")
+# dataloader.AddVariable("jet2_eta","F") # 15
+# dataloader.AddVariable("jet2_phi","F") # 15
+# dataloader.AddVariable("jet2_btag","F") # 15
+# dataloader.AddVariable("jet3_pt","F") # 10
+# dataloader.AddVariable("jet3_eta","F") # 10
+# dataloader.AddVariable("jet3_phi","F") # 15
+# dataloader.AddVariable("jet3_btag","F") # 15
+# dataloader.AddVariable("jet4_pt","F") # 10
+# dataloader.AddVariable("jet4_eta","F") # 15
+# dataloader.AddVariable("jet4_phi","F") # 15
+# dataloader.AddVariable("jet4_btag","F") # 15
+# dataloader.AddVariable("m_jj","F") # corr 2 # 9
+dataloader.AddVariable("m_jjj","F")
+# dataloader.AddVariable("m_lv","F") # 10
+dataloader.AddVariable("m_jlv","F")
+dataloader.AddVariable("m_bb","F")
+dataloader.AddVariable("m_wbb","F") # corr 1
+dataloader.AddVariable("m_wwbb","F") 
 
 sig_weight = 1.
 bkg_weight = 1.
@@ -35,16 +49,14 @@ bkg_weight = 1.
 dataloader.AddSignalTree(tree_sig, sig_weight)
 dataloader.AddBackgroundTree(tree_bkg, bkg_weight)
 
-#dataloader.SetWeightExpression("weight")
+# dataloader.SetWeightExpression("weight")
 
-# eventuali tagli (tipo mjj>8)
 mycutSig = ROOT.TCut("")
 mycutBkg = ROOT.TCut("")
 
-#queste righe applicano il BDT
-dataloader.PrepareTrainingAndTestTree(mycutSig, mycutBkg, ":".join(["!V","nTrain_Signal=0:nTrain_Background=0:nTest_Signal=0:nTest_Background=0"])) 
-method_btd  = factory.BookMethod(dataloader, ROOT.TMVA.Types.kBDT, "BDT", ":".join(["H","!V","NTrees=800", "MinNodeSize=2.5%","MaxDepth=3","BoostType=AdaBoost","AdaBoostBeta=0.25","nCuts=20","NegWeightTreatment=IgnoreNegWeightsInTraining"]))
+dataloader.PrepareTrainingAndTestTree(mycutSig, mycutBkg, ":".join(["!V","nTrain_Signal=7000:nTrain_Background=7000:nTest_Signal=0:nTest_Background=0"]))
 
+method_btd  = factory.BookMethod(dataloader, ROOT.TMVA.Types.kBDT, "BDT", ":".join(["H","!V","NTrees=500", "MinNodeSize=2.5%","MaxDepth=3","BoostType=AdaBoost","AdaBoostBeta=0.4","nCuts=30","NegWeightTreatment=IgnoreNegWeightsInTraining"]))
 
 factory.TrainAllMethods()
 factory.TestAllMethods()
@@ -52,5 +64,4 @@ factory.EvaluateAllMethods()
 
 fOut.Close()
 
-weightfile_dir = "default/weights/TMVAClassification_BDT.weights.xml"
-
+weightfile_dir = "TMVAClassification_BDT.weights.xml"
